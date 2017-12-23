035674d5c96d4d42831e88468fe2384eb3a0a039
Andy <andy@Andys-MacBook-Air.local>
2017-12-22 21:31:28 -0800

    added /send-button
    added interactive messages to /send-button command

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // Import library to manage .env variables
 require('dotenv').config();
 
-// Import express and request modules
+
+// Import express and request modules and urlencoded parsers
 var express = require('express');
 var request = require('request');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({limit: '10mb', extended: true});
 
 // Store our app's ID and Secret. These we got from Step 1. 
 // For this tutorial, we'll keep your API credentials right here. But for an actual app, you'll want to  store them securely in environment variables. 
-var clientId = '160263341057.290711221763';
-var clientSecret = 'edc6878f8088d5c6ba4d7d8702b6a541';
-
 var clientId = process.env.SLACKAPP_CLIENT_ID;
 var clientSecret = process.env.SLACKAPP_CLIENT_SECRET;
 
-// Instantiates Express and assigns our app variable to it
 var app = express();
-
-
-// Again, we define a port we want to listen to
 const PORT=process.env.PORT;
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+
 
 // Lets start our server
 app.listen(PORT, function () {
@@ -63,6 +63,79 @@ app.get('/oauth', function(req, res) {
 app.post('/command', function(req, res) {
     res.send('Your ngrok tunnel is up and running!');
 });
+
+app.post('/send-button', urlencodedParser, (req, res) =>{
+    res.status(200).end() // best practice to respond with empty 200 status code
+    var reqBody = req.body
+    var responseURL = reqBody.response_url
+    if (reqBody.token != process.env.SLACK_VERIFICATION_TOKEN){
+        res.status(403).end("Access forbidden");
+    }else{
+        var message = {
+            "text": "This is your first interactive message",
+            "attachments": [
+                {
+                    "text": "Building buttons is easy right?",
+                    "fallback": "Shame... buttons aren't supported in this land",
+                    "callback_id": "button_tutorial",
+                    "color": "#3AA3E3",
+                    "attachment_type": "default",
+                    "actions": [
+                        {
+                            "name": "yes",
+                            "text": "yes",
+                            "type": "button",
+                            "value": "yes"
+                        },
+                        {
+                            "name": "no",
+                            "text": "no",
+                            "type": "button",
+                            "value": "no"
+                        },
+                        {
+                            "name": "maybe",
+                            "text": "maybe",
+                            "type": "button",
+                            "value": "maybe",
+                            "style": "danger"
+                        }
+                    ]
+                }
+            ]
+        }
+        sendMessageToSlackResponseURL(responseURL, message)
+    }
+})
+
+app.post('/actions', urlencodedParser, (req, res) =>{
+    res.status(200).end() // best practice to respond with 200 status
+    var actionJSONPayload = JSON.parse(req.body.payload) // parse URL-encoded payload JSON string
+    var message = {
+        "text": actionJSONPayload.user.name+" clicked: "+actionJSONPayload.actions[0].name,
+        "replace_original": false
+    }
+    sendMessageToSlackResponseURL(actionJSONPayload.response_url, message)
+})
+
+
+function sendMessageToSlackResponseURL(responseURL, JSONmessage){
+    var postOptions = {
+        uri: responseURL,
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        json: JSONmessage
+    }
+    request(postOptions, (error, response, body) => {
+        if (error){
+        	console.log(error);
+            // handle errors as you see fit
+        }
+    })
+}
+
 
 
 
